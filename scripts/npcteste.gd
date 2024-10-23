f6a3f3cf02ff69fc81d168a98d70209c853da986
AdrianoMonteagudo <adriano.monteagudo@aluno.ifsp.edu.br>
2024-10-23 20:57:29 -0300

    Dialogo

--- scripts/npcteste.gd
+++ scripts/npcteste.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @onready var interação_area: InteraçãoArea = $"interação_area"
 
+const Lines: Array[String] = [
+	"Me mata pfv aaaaaa"
+]
 
 func _ready() -> void:
 	interação_area.interagir = Callable(self, "interagindo")
 	
 func interagindo():
-	print ("funcionapfv")
+	Gerentedialogo.inicia_dialogo(global_position, Lines)
+	await Gerentedialogo.dialogo_acaba
